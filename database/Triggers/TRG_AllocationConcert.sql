87ba290b25f62496d68702e70a86c03d8bbcc8a2
TramMaiQuynh <trammaiquynh@github.local>
2026-08-25 03:00:32 +0700

    feat(db): viết trigger TRG_AllocationConcert

---
+++ database/Triggers/TRG_AllocationConcert.sql
@@ -0,0 +1,19 @@
+CREATE TRIGGER TRG_AllocationConcert ON BookingEventSeatAllocation
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    IF EXISTS (
+        SELECT 1
+        FROM inserted i
+        JOIN Booking b ON i.BookingID = b.BookingID
+        JOIN EventSeat es ON i.EventSeatID = es.EventSeatID
+        WHERE b.ConcertID <> es.ConcertID
+    )
+    BEGIN
+        ROLLBACK TRANSACTION;
+        THROW 50000, 'DR-10/CI03 Violation: EventSeat của Allocation phải thuộc cùng Concert với Booking.', 1;
+    END
+END;
+GO
