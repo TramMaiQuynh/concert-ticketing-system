99c130e187ec986f1fd7b260c372679db499471a
TramMaiQuynh <trammaiquynh@github.local>
2026-08-31 04:27:00 +0700

    test: khởi tạo dự án unit test và integration test

---
+++ backend/tests/ConcertTicketing.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ConcertTicketing.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ backend/tests/ConcertTicketing.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ConcertTicketing.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
